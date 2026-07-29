0af3d1fce6149ee6127fcac0b017d94b6a8569d5
gurkha231224-netizen <gurkha231224@gmail.com>
2026-07-29 15:26:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-function bookTicket() {
-    alert("Ticket booking system coming in the next update!");
+import { db } from "./firebase.js";
+
+import {
+  collection,
+  getDocs,
+  query,
+  orderBy,
+  limit
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+async function loadGame() {
+
+  const q = query(
+    collection(db, "games"),
+    orderBy("createdAt", "desc"),
+    limit(1)
+  );
+
+  const snapshot = await getDocs(q);
+
+  if (snapshot.empty) {
+    document.getElementById("game").innerHTML =
+      "<h3>No game available</h3>";
+    return;
+  }
+
+  const game = snapshot.docs[0].data();
+
+  document.getElementById("game").innerHTML = `
+    <h2>${game.name}</h2>
+    <p>Ticket Price: ₹${game.price}</p>
+    <p>Total Tickets: ${game.tickets}</p>
+    <p>Date: ${game.date}</p>
+    <p>Time: ${game.time}</p>
+  `;
+
+  document.getElementById("bookBtn").href =
+    "https://wa.me/" +
+    game.whatsapp +
+    "?text=I want to book a Lucky 90 Tambola ticket.";
 }
+
+loadGame();
